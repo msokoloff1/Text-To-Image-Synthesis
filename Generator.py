ecebd4c72b3ab1399ccf875de104e5e2d485a78a
Matt <matthewsokoloff@gmail.com>
2017-05-05 12:23:43 -0400

    Finished feedforward code, need to make reusable and create loss function

--- Generator.py
+++ Generator.py
@@ -10,31 +10,70 @@ class Generator(Network):
 		self.batchSize = batchSize
 		self.textManager = textManager
 		self.noisePH = tf.placeholder(tf.float32, shape = [None, 100])
-		self.net = self._createNetwork()
+		with tf.variable_scope("GeneratorVars"):
+			self.net = self._createNetwork()
 
 	def _createNetwork(self):
 
 
 		input = tf.concat([self.noisePH, self.textManager.getOutputEmbedding()], axis = 1)
-		reshaped = tf.reshape(input, [self.batchSize,1,1,200])
+		print(input.get_shape())
+		reshaped = tf.reshape(input, [self.batchSize,1,1,300])
 		print(reshaped.get_shape())
 		layer1 = self._deconvLayer(inputLayer = reshaped
 					  , filterWH = 5
-					  , numOutputFeatures =  256
+					  , numOutputFeatures =  250
 					  , stride =  2
 					  , batchSize = self.batchSize 
 					  , name = "dconv1")
 		print(layer1.get_shape())
 		layer2 = self._deconvLayer(inputLayer = layer1
                                           , filterWH = 5
-                                          , numOutputFeatures =  256
+                                          , numOutputFeatures =  220
                                           , stride =  2
                                           , batchSize = self.batchSize
-                                          , name = "dconv1")
+                                          , name = "dconv2")
 		print(layer2.get_shape())
-		#layer2 = self._deconvLayer()
+
+		layer3 = self._deconvLayer(inputLayer = layer2
+                                          , filterWH = 5
+                                          , numOutputFeatures =  80
+                                          , stride =  2
+                                          , batchSize = self.batchSize
+                                          , name = "dconv3")
+		print(layer3.get_shape())
+
+		layer4 = self._deconvLayer(inputLayer = layer3
+                                          , filterWH = 5
+                                          , numOutputFeatures =  30
+                                          , stride =  2
+                                          , batchSize = self.batchSize
+                                          , name = "dconv4")
+		print(layer4.get_shape())
+
+
+		layer5 = self._deconvLayer(inputLayer = layer4
+                                          , filterWH = 5
+                                          , numOutputFeatures =  10
+                                          , stride =  2
+                                          , batchSize = self.batchSize
+                                          , name = "dconv5")
+		print(layer5.get_shape())
+
+
+		layer6 = self._deconvLayer(inputLayer = layer5
+                                          , filterWH = 5
+                                          , numOutputFeatures =  3
+                                          , stride =  2
+                                          , batchSize = self.batchSize
+                                          , name = "dconv6"
+					  , activation = tf.nn.tanh
+					  , norm = False)
+		print(layer6.get_shape())
+		self.output = (layer6/2.) + 0.5
 
 
 model = TextModel(200)
 gen = Generator(model,1)
+
 
--- Network.py
+++ Network.py
@@ -2,20 +2,41 @@
 import numpy as np
 import tensorflow as tf
 class Network():
+	def _convLayer(self, inputLayer, filterWH, numOutputFeatures, strideSize, name, activation= tf.nn.relu, padType = 'VALID'):
+		numInputFeatures = inputLayer.get_shape().as_list()[-1]    
+		convFilter = tf.Variable(tf.random_normal([filterWH, filterWH, numInputFeatures, numOutputFeatures], stddev=np.sqrt(2./( (filterWH**2)*numInputFeatures)), name="weights"))
+		strideConfig =  [1, strideSize, strideSize, 1]
+                
+		conv = tf.nn.conv2d(inputLayer, convFilter, strideConfig, padding=padType)
+		bias = tf.Variable(tf.zeros([numOutputFeatures]), name="biases")
+		convOutput = tf.nn.bias_add(conv, bias)
+
+		if activation == tf.nn.relu:
+			norm = self._normalizeBatch(convOutput)
+			return activation(norm)
+		else:
+			return activation(convOutput)
+
 	def _residualBlock(self, inputLayer, name, filterWH=3):
 		with tf.variable_scope(name):
 			numInputFeatures = inputLayer.get_shape().as_list()[-1]
-			conv = self.__convLayer__(inputLayer, filterWH, numInputFeatures, 1, "%s-a" % name)
-			return inputLayer + self.__normalizeBatch__(self.__convLayer__(conv, filterWH, numInputFeatures, 1, name, activation=lambda x: x))
+			conv = self._convLayer(inputLayer, filterWH, numInputFeatures, 1, "%s-a" % name)
+			return inputLayer + self._normalizeBatch(self._convLayer(conv, filterWH, numInputFeatures, 1, name, activation=lambda x: x))
 
-	def _normalizeBatch(self,x):
-		with tf.variable_scope("instance_norm"):
-			eps = 1e-6
-			mean, var = tf.nn.moments(x, [1, 2], keep_dims=True)
-			return (x - mean) / (tf.sqrt(var) + eps)
+	#def _normalizeBatch(self,x):
+	#	with tf.variable_scope("instance_norm"):
+	#		eps = 1e-6
+	#		mean, var = tf.nn.moments(x, [1, 2], keep_dims=True)
+	#		return (x - mean) / (tf.sqrt(var) + eps)
+	def _normalizeBatch(self, x):
+		with tf.variable_scope("batch_norm"):
+			mean = tf.Variable(tf.constant(0.1 ,shape = [1]))
+			variance = tf.Variable(tf.constant(0.1, shape = [1]))
+			beta = tf.Variable(tf.constant(0.1 ,shape = [1]))
+			gamma = tf.Variable(tf.constant(0.1, shape = [1]))
+			return tf.nn.batch_normalization(x,mean, variance,beta, gamma, 0.00001) 
 
-
-	def _deconvLayer(self, inputLayer, filterWH, numOutputFeatures, stride,batchSize, name):
+	def _deconvLayer(self, inputLayer, filterWH, numOutputFeatures, stride,batchSize, name, activation = tf.nn.relu, norm = True):
 		with tf.variable_scope(name):
 			_, h, w, numInputFeatures = inputLayer.get_shape().as_list()
             
@@ -23,7 +44,8 @@ class Network():
 			deconv = tf.nn.conv2d_transpose(inputLayer, convFilter, [batchSize, h * stride, w * stride, numOutputFeatures], [1, stride, stride, 1], padding='SAME')
 			biases = tf.Variable(tf.zeros([numOutputFeatures]), name="biases")
 			bias = tf.nn.bias_add(deconv, biases)
-			norm = self._normalizeBatch(bias)
-			return tf.nn.relu(norm)
-        
+			if(norm):
+				return activation(self._normalizeBatch(bias))
+
+			return activation(bias) 
 
